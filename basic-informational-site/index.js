d37a938459c000b95ef02653225335cc9331eb8b
Ali Fadaei <imfadaei2008@gmail.com>
2026-08-31 09:10:24 +0330

    my first node project

---
+++ basic-informational-site/index.js
@@ -0,0 +1,30 @@
+import http from "http";
+import { readFile } from "node:fs/promises";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/") {
+    const html = await readFile("./index.html", "utf8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else if (method === "GET" && url === "/about") {
+    const html = await readFile("./about.html", "utf8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else if (method === "GET" && url === "/contact-me") {
+    const html = await readFile("./contact-me.html", "utf8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  } else {
+    const html = await readFile("./404.html", "utf8");
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(html);
+  }
+});
+
+server.listen(8080);

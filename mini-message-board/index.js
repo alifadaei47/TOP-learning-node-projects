2826174b59009fc12e56932dd64b66a1e2ba19b1
Ali Fadaei <imfadaei2008@gmail.com>
2026-08-31 11:01:03 +0330

    add .env file

--- mini-message-board/index.js
+++ mini-message-board/index.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import express from "express";
 import ejs from "ejs";
 
 const app = express();
-const port = 3000;
+const port = +process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

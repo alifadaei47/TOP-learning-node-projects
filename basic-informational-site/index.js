88b9ea675b058da8b56a1b3ef7d7dd3df8280581
Ali Fadaei <imfadaei2008@gmail.com>
2026-08-31 09:15:42 +0330

    create views folder

--- basic-informational-site/index.js
+++ basic-informational-site/index.js
@@ -9,19 +9,19 @@ const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/") {
-    const html = await readFile("./index.html", "utf8");
+    const html = await readFile("./views/index.html", "utf8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
   } else if (method === "GET" && url === "/about") {
-    const html = await readFile("./about.html", "utf8");
+    const html = await readFile("./views/about.html", "utf8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
   } else if (method === "GET" && url === "/contact-me") {
-    const html = await readFile("./contact-me.html", "utf8");
+    const html = await readFile("./views/contact-me.html", "utf8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
   } else {
-    const html = await readFile("./404.html", "utf8");
+    const html = await readFile("./views/404.html", "utf8");
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end(html);
   }

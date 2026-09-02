634025cd01fd867d759063f54dd809f4ebcd1cc0
Ali Fadaei <imfadaei2008@gmail.com>
2026-09-02 14:17:27 +0330

    improve styles and add public folder

--- mini-message-board/app.js
+++ mini-message-board/app.js
@@ -16,6 +16,10 @@ const port = +process.env.PORT ?? 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);

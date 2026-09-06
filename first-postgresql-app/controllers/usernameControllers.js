01f4fe04943db4d1dc12cece46bdc5ada2f9902a
Ali Fadaei <imfadaei2008@gmail.com>
2026-09-06 11:59:41 +0330

    finish basic database for usernames

--- first-postgresql-app/controllers/usernameControllers.js
+++ first-postgresql-app/controllers/usernameControllers.js
@@ -1,12 +1,17 @@
-export const getUsernames = (req, res) => {
-  console.log("usernames will be logged here - wip");
-  return;
+import * as db from "../db/queries.js";
+
+export const getUsernames = async function (req, res) {
+  const usernames = await db.getAllUsernames();
+  console.log("Usernames: ", usernames);
+  res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
 };
 
-export const newUsernameGet = (req, res) => {
-  res.render("new");
+export const createUsernameGet = async function (req, res) {
+  res.render("createUsername");
 };
 
-export const newUsernamePost = (req, res) => {
-  console.log("username to be saved: ", req.body.username);
+export const createUsernamePost = async function (req, res) {
+  const { username } = req.body;
+  await db.insertUsername(username);
+  res.redirect("/");
 };
